db45fc3871b75638968ff65fd79c1551330ec912
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-07-26 18:18:29 +0100

    routing

--- alx-project-0x03/pages/_app.tsx
+++ alx-project-0x03/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Header from "@/components/layouts/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Footer from "@/components/layouts/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+  <Header />
+  <Component {...pageProps} />;
+  <Footer />
+  </>
+  )
 }
