929c53bafbb262d34f4170ea084fd9eb51ba228b
Azzeddine Iqachaden <pandonyx@gmail.com>
2025-07-31 19:21:15 +0100

    context

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,14 +1,14 @@
-import Header from "@/components/layouts/Header";
+import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Footer from "@/components/layouts/Footer";
+import { CountProvider } from "@/context/CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-  <Header />
-  <Component {...pageProps} />;
-  <Footer />
-  </>
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
   )
 }
